58a2e8511d2162a25864c2266a066a762e37970f
JSheleg <jessica.sheleg@gmail.com>
2021-06-04 12:01:29 -0500

    UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -69,6 +69,7 @@ var auditTask = function(taskEl){
   else if(Math.abs(moment().diff(time,"days"))<=2){
     $(taskEl).addClass("list-group-item-warning");
   }
+  console.log(taskEl)
 };
 
 //enable draggable/sortable feature on list-group elements
@@ -79,15 +80,21 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function(event, ui) {
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
     console.log(ui);
   },
   deactivate: function(event, ui) {
+    $(this).addClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
     console.log(ui);
   },
   over: function(event) {
+    $(event.target).addClass("dropover-active");
     console.log(event);
   },
   out: function(event) {
+    $(event.target).removeClass("dropover-active");
     console.log(event);
   },
   update: function() {
@@ -131,12 +138,15 @@ $("#trash").droppable({
   drop: function(event, ui) {
     //remove dragged elemnt from the dom
     ui.draggable.remove();
+    $(".bottom-trash").removeClass("bottom-trash-active");
     
   },
   over: function(event, ui) {
+    $(".bottom-trash").addClass("bottom-trash-active");
     console.log("over");
   },
   out: function(event, ui) {
+    $(".bottom-trash").removeClass("bottom-trash-active");
     console.log("out");
   }
   
@@ -155,7 +165,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -300,5 +310,8 @@ $("#remove-tasks").on("click", function() {
 loadTasks();
 
 
-
-
+setInterval(function () {
+  $(".card .list-group-item").each(function(index, el) {
+    auditTask(el);
+  });
+}, (1000*60)*30);
